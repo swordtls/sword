681b6ddd2ac619a6b71b9e2041f76a1287b498a2
sword <swordtls@mail.com>
2020-02-26 08:34:32 -0800

    bug fix

--- core/proxy_server.go
+++ core/proxy_server.go
@@ -3,8 +3,10 @@ package core
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"sword/conf"
 )
 
@@ -43,7 +45,27 @@ func ProxyServer(filename string) {
 			log.Println(err)
 			continue
 		}
-		go DoClientProxy(conn, serverConfig.RedirectAddress)
+		go DoClientProxyNoTls(conn, serverConfig.RedirectAddress)
 
 	}
+}
+
+func DoClientProxyNoTls(conn net.Conn, serverAddress string) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println(err)
+			return
+		}
+	}()
+	defer conn.Close()
+	var err error
+	var server net.Conn
+	server, err = net.Dial("tcp", serverAddress)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer server.Close()
+	go io.Copy(server, conn)
+	io.Copy(conn, server)
 }
